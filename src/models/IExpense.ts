3a873b089ab738247c44d9af79f5fc7cf54fe3cc
Acrylic125 <acrylic125email@gmail.com>
2023-03-07 20:53:48 +0800

    refactor: reformat

--- src/models/IExpense.ts
+++ src/models/IExpense.ts
@@ -1,12 +1,11 @@
-
 export interface IExpense {
-    id?: string;
-    title: string;
-    amount: number;
-    happenedAt: Date;
-    imageUrl?:string;
-    imagePublicId?:string;
-    originalFileName?:string; 
-    imageWidth?:number;
-    imageHeight?:number;   
+  id?: string;
+  title: string;
+  amount: number;
+  happenedAt: Date;
+  imageUrl?: string;
+  imagePublicId?: string;
+  originalFileName?: string;
+  imageWidth?: number;
+  imageHeight?: number;
 }
