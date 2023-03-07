0fbf13fabaadc507650e2f0c65639aea53d81abc
Acrylic125 <acrylic125email@gmail.com>
2023-03-07 20:53:58 +0800

    refactor: use base

--- src/pages/api/expenses/index.ts
+++ src/pages/api/expenses/index.ts
@@ -1,11 +1,11 @@
+import base from "@/middlewares/common";
+import { IExpense } from "@/models/IExpense";
+import { IFileUploadResult } from "@/models/IFileUploadResult";
+import { uploadStreamToCloudinary } from "@/utils/cloudinary";
+import multer from "multer";
 import { NextApiRequest, NextApiResponse } from "next";
 import { conn } from "src/utils/database";
 import { ApiResponse } from "../../../models/ApiResponse";
-import nextConnect from "next-connect";
-import multer from "multer";
-import { uploadStreamToCloudinary } from "@/utils/cloudinary";
-import { IFileUploadResult } from "@/models/IFileUploadResult";
-import { IExpense } from "@/models/IExpense";
 
 //The bulk of the code in index.ts has been  influenced by the content at:
 //https://github.com/codeBelt/my-next.js-starter/blob/upload/src/pages/api/uploads/index.ts
@@ -28,21 +28,22 @@ const upload = multer({
   limits: { fileSize: oneMegabyteInBytes * 2 },
   storage: multer.memoryStorage(),
 });
-const apiRoute = nextConnect({
-  onError(error, req: INextConnectApiRequest, res: NextApiResponse<ResponseData>) {
-    //You will only be able to inspect the request structure when error occurs
-    console.log(`apiRoute [onError] handler logic has executed.`);
-    console.log(`inspect [req]`);
-    console.log(req);
-    //<TODO>
-    //I should not be responding the details of the error to the client-side. There must be another way to log it somewhere
-    //. This NextJS project does not have proper error logging logic in place.
-    res.status(501).json({ error: `System has encountered error ${error.message}`, statusCode: 501, message: error.message });
-  },
-  onNoMatch(req: INextConnectApiRequest, res: NextApiResponse<ResponseData>) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed`, statusCode: 405, message: `Method '${req.method}' Not Allowed` });
-  },
-});
+// const apiRoute = nextConnect({
+//   onError(error, req: INextConnectApiRequest, res: NextApiResponse<ResponseData>) {
+//     //You will only be able to inspect the request structure when error occurs
+//     console.log(`apiRoute [onError] handler logic has executed.`);
+//     console.log(`inspect [req]`);
+//     console.log(req);
+//     //<TODO>
+//     //I should not be responding the details of the error to the client-side. There must be another way to log it somewhere
+//     //. This NextJS project does not have proper error logging logic in place.
+//     res.status(501).json({ error: `System has encountered error ${error.message}`, statusCode: 501, message: error.message });
+//   },
+//   onNoMatch(req: INextConnectApiRequest, res: NextApiResponse<ResponseData>) {
+//     res.status(405).json({ error: `Method '${req.method}' Not Allowed`, statusCode: 405, message: `Method '${req.method}' Not Allowed` });
+//   },
+// });
+const apiRoute = base<unknown[], INextConnectApiRequest>();
 
 apiRoute.use(upload.array("file", 1));
 
