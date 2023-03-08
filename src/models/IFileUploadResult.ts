11b635688c8971679316a46c82c9b72edad97da4
Acrylic125 <acrylic125email@gmail.com>
2023-03-08 09:13:33 +0800

    refactor: reformat

--- src/models/IFileUploadResult.ts
+++ src/models/IFileUploadResult.ts
@@ -1,10 +1,9 @@
-
 export interface IFileUploadResult {
-    publicId?: string;
-    url?: string;
-    status: string;
-    error?:any;
-    width?:number;
-    height?:number;
-    originalFileName?:string;
+  publicId?: string;
+  url?: string;
+  status: string;
+  error?: any;
+  width?: number;
+  height?: number;
+  originalFileName?: string;
 }
